7d9c712e871ccfcd262e3362c19b4463f548cb15
Nienke Dekker <nienke.dekker@nos.nl>
2020-10-04 19:31:59 +0200

    add todos to create.js

--- pages/api/item/create.js
+++ pages/api/item/create.js
@@ -1,40 +1,17 @@
-import { useState } from 'react';
-import Router from 'next/router';
-import { useForm } from 'react-hook-form';
-
-const CreateItem = (type) => {
-  const [errorMessage, setErrorMessage] = useState('');
-  const { handleSubmit, register, errors } = useForm();
-
-  const onSubmit = handleSubmit(async (formData) => {
-    if (errorMessage) setErrorMessage('');
-
-    try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${type}`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: {
-          // todo
-          Authorization: `Bearer ${Cookie.get('mevn-token')}`,
-        },
-        body: JSON.stringify(formData),
-      });
-      if (res.status === 200) {
-        Router.push('/');
-      } else {
-        throw new Error(await res.text());
-      }
-    } catch (error) {
-      console.error(error);
-      setErrorMessage(error.message);
-    }
-  });
-
-  return (
-    <>
-      <p>TODO</p>
-    </>
-  );
-};
-
-export default CreateItem;
+export async function CreateItem(req, res) {
+  const { type, newItem } = req.body;
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${type}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        // todo
+        Authorization: `Bearer ${Cookie.get('mevn-token')}`,
+      },
+      body: JSON.stringify(newItem),
+    });
+    res.status(200).end();
+  } catch (error) {
+    res.status(500).json({ error: e.message });
+  }
+}
