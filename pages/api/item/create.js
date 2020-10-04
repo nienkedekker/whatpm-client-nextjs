4f41b14dbc381162ed421c11eef5851ea86eef27
Nienke Dekker <nienke.dekker@nos.nl>
2020-10-04 19:24:41 +0200

    add todos to create.js

--- pages/api/item/create.js
+++ pages/api/item/create.js
@@ -2,9 +2,8 @@ import { useState } from 'react';
 import Router from 'next/router';
 import { useForm } from 'react-hook-form';
 
-const Create = (type) => {
+const CreateItem = (type) => {
   const [errorMessage, setErrorMessage] = useState('');
-
   const { handleSubmit, register, errors } = useForm();
 
   const onSubmit = handleSubmit(async (formData) => {
@@ -15,6 +14,7 @@ const Create = (type) => {
         method: 'POST',
         credentials: 'include',
         headers: {
+          // todo
           Authorization: `Bearer ${Cookie.get('mevn-token')}`,
         },
         body: JSON.stringify(formData),
@@ -32,83 +32,9 @@ const Create = (type) => {
 
   return (
     <>
-      <h1>Create Customer</h1>
-
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>First Name</label>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="e.g. John"
-            ref={register({ required: 'First Name is required' })}
-          />
-          {errors.firstName && (
-            <span role="alert" className="error">
-              {errors.firstName.message}
-            </span>
-          )}
-        </div>
-
-        <div>
-          <label>Last Name</label>
-          <input
-            type="text"
-            name="lastName"
-            placeholder="e.g. Doe"
-            ref={register({ required: 'Last Name is required' })}
-          />
-          {errors.lastName && (
-            <span role="alert" className="error">
-              {errors.lastName.message}
-            </span>
-          )}
-        </div>
-
-        <div>
-          <label>Telephone</label>
-          <input
-            type="text"
-            name="telephone"
-            placeholder="e.g. 123-456-7890"
-            ref={register}
-          />
-          {errors.telephone && (
-            <span role="alert" className="error">
-              {errors.telephone.message}
-            </span>
-          )}
-        </div>
-
-        <div>
-          <label>Credit Card Number</label>
-          <input
-            type="text"
-            name="creditCardNumber"
-            placeholder="e.g. 1234567890123456"
-            ref={register}
-          />
-          {errors.creditCardNumber && (
-            <span role="alert" className="error">
-              {errors.creditCardNumber.message}
-            </span>
-          )}
-        </div>
-
-        <div className="submit">
-          <button type="submit" className="createButton">
-            Create
-          </button>
-        </div>
-      </form>
-
-      {errorMessage && (
-        <p role="alert" className="errorMessage">
-          {errorMessage}
-        </p>
-      )}
+      <p>TODO</p>
     </>
   );
 };
 
-export default Create;
+export default CreateItem;
